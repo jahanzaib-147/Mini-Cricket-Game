038d96c01cea1802d0e98033059c8f8843a9b1e2
Muhammad Jahanzaib <126382092+jahanzaib-147@users.noreply.github.com>
2024-03-19 22:48:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Mini Cricket Game");
+var random=Math.floor(Math.random() * 7);
+alert("Reload Again & Again To Hit ");
+if(random===0){
+    alert("You Are Out : " + random)
+}
+else{
+    alert(random);
+}
